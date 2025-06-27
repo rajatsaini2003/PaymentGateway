96df7f8111d9c69cc7de36d66357eba1510f0245
rajatsaini2003 <rajatsaini9460@gmail.com>
2025-06-28 00:00:48 +0530

    cors:update

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'https://payment-gateway-ruby-xi.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
